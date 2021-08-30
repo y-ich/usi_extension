03e68e4848f58d665c7212573766ed7be3fdf192
y-ich <ichikawa.yuji@gmail.com>
2021-08-30 14:36:58 +0900

    check existence of script

--- usi_extension/content.js
+++ usi_extension/content.js
@@ -47,46 +47,48 @@ function addSituationBar(container) {
 }
 
 const script = document.querySelector("main > script");
-for (const line of script.textContent.split("\n")) {
-    const match = line.match(/const KIF_FILE_NAME = "(.*)";/);
-    if (match) {
-        const container = document.querySelector("div");
-        const [bar, white, black] = addSituationBar(container);
-        let thinking = false;
-        bar.addEventListener("click", function(event) {
-            if (thinking) {
-                chrome.runtime.sendMessage({
-                    command: "stop"
-                });
-            } else {
-                const url = new URL(match[1], document.location);
-                chrome.runtime.sendMessage({
-                    command: "think",
-                    url: url.toString()
-                });
-            }
-            thinking = !thinking;
-            bar.classList.toggle("thinking");
-        }, false);
-        chrome.runtime.onMessage.addListener(function(message, sender, callback) {
-            console.log(message);
-            if (!("score cp" in message && "pv" in message && "nps" in message)) {
-                return;
-            }
-            const winrate = Math.round(scoreToWinrate(message["score cp"]) * 100);
-            const move = japaneseMove(message["pv"][0].toLowerCase());
-            if (message.moveNumber % 2 === 0) {
-                white.textContent = `${100 - winrate}%`;
-                white.style.width = `${100 - winrate}%`;
-                black.textContent = `${winrate}% ${message.moveNumber + 1}手目 ${move} ${message["nps"]}nps`;
-                black.style.width = `${winrate}%`;
-            } else {
-                white.textContent = `${winrate}% ${message.moveNumber + 1}手目 ${move} ${message["nps"]}nps`;
-                white.style.width = `${winrate}%`;
-                black.textContent = `${100 - winrate}%`;
-                black.style.width = `${100 - winrate}%`;
-            }
-        });
-        break;
+if (script != null) {
+    for (const line of script.textContent.split("\n")) {
+        const match = line.match(/const KIF_FILE_NAME = "(.*)";/);
+        if (match) {
+            const container = document.querySelector("div");
+            const [bar, white, black] = addSituationBar(container);
+            let thinking = false;
+            bar.addEventListener("click", function(event) {
+                if (thinking) {
+                    chrome.runtime.sendMessage({
+                        command: "stop"
+                    });
+                } else {
+                    const url = new URL(match[1], document.location);
+                    chrome.runtime.sendMessage({
+                        command: "think",
+                        url: url.toString()
+                    });
+                }
+                thinking = !thinking;
+                bar.classList.toggle("thinking");
+            }, false);
+            chrome.runtime.onMessage.addListener(function(message, sender, callback) {
+                console.log(message);
+                if (!("score cp" in message && "pv" in message && "nps" in message)) {
+                    return;
+                }
+                const winrate = Math.round(scoreToWinrate(message["score cp"]) * 100);
+                const move = japaneseMove(message["pv"][0].toLowerCase());
+                if (message.moveNumber % 2 === 0) {
+                    white.textContent = `${100 - winrate}%`;
+                    white.style.width = `${100 - winrate}%`;
+                    black.textContent = `${winrate}% ${message.moveNumber + 1}手目 ${move} ${message["nps"]}nps`;
+                    black.style.width = `${winrate}%`;
+                } else {
+                    white.textContent = `${winrate}% ${message.moveNumber + 1}手目 ${move} ${message["nps"]}nps`;
+                    white.style.width = `${winrate}%`;
+                    black.textContent = `${100 - winrate}%`;
+                    black.style.width = `${100 - winrate}%`;
+                }
+            });
+            break;
+        }
     }
 }
